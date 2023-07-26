939e2fededd124be0783e3e44ce7494e4502c143
Lauren Miller <lmiller90@fordham.edu>
2023-07-26 16:26:44 -0400

    wrote out timer function

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -140,22 +140,15 @@ function loseGame() {
 // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
 function startTimer() {
   // Sets timer
-  timer = setInterval(function() {
+  var timer = setInterval(function() {
     timerCount--;
     timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      if (isWin && timerCount > 0) {
-        // Clears interval and stops timer
-        clearInterval(timer);
-        winGame();
-      }
-    }
+    
     // Tests if time has run out
     if (timerCount === 0) {
       // Clears interval
       clearInterval(timer);
-      loseGame();
+      sendMessage();
     }
   }, 1000);
 }
@@ -264,7 +257,7 @@ document.addEventListener("keydown", function(event) {
 });
 
 // Attach event listener to start button to call startGame function on click
-startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", startQuiz);
 
 // Calls init() so that it fires when page opened
 init();
